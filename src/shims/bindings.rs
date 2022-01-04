c966a22b27b0286e701f2f09d79be0d36200362b
Steve Whittle <narfit66@googlemail.com>
2022-01-04 18:17:48 +0000

    undated copyright date

--- src/shims/bindings.rs
+++ src/shims/bindings.rs
@@ -1,7 +1,7 @@
 /*
     src/shims/bindings.rs
 
-    Copyright (c) 2019-2021 Stephen Whittle  All rights reserved.
+    Copyright (c) 2019-2022 Stephen Whittle  All rights reserved.
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"),
